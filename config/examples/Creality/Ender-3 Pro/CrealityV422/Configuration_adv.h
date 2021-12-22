50e9ce9f902ec7260eb275616001d30043cd3d2a
Oncleben31 <oncleben31@users.noreply.github.com>
2021-12-23 00:20:13 +0100

    Enable the Binary File Transfer protocol
    To be used with Octoprint FirmwareUpdater plugin.
    Documentation: https://github.com/OctoPrint/OctoPrint-FirmwareUpdater/blob/master/doc/marlinbft.md

--- config/examples/Creality/Ender-3 Pro/CrealityV422/Configuration_adv.h
+++ config/examples/Creality/Ender-3 Pro/CrealityV422/Configuration_adv.h
@@ -1727,7 +1727,7 @@
   //#define CONFIGURATION_EMBEDDING
 
   // Add an optimized binary file transfer mode, initiated with 'M28 B1'
-  //#define BINARY_FILE_TRANSFER
+  #define BINARY_FILE_TRANSFER
 
   #if ENABLED(BINARY_FILE_TRANSFER)
     // Include extra facilities (e.g., 'M20 F') supporting firmware upload via BINARY_FILE_TRANSFER
@@ -1743,7 +1743,7 @@
    *
    * :[ 'LCD', 'ONBOARD', 'CUSTOM_CABLE' ]
    */
-  //#define SDCARD_CONNECTION LCD
+  #define SDCARD_CONNECTION ONBOARD
 
   // Enable if SD detect is rendered useless (e.g., by using an SD extender)
   //#define NO_SD_DETECT
